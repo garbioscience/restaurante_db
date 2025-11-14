4a83c006a97a6150dcb8c7c4eec959f936e3dfb9
Jairsiño <jairbarrios686@gmail.com>
2025-11-14 17:09:43 -0500

    Add files via upload
    archivo mysql con cliente y factura

---
+++ tablas/cliente & factura.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-11-2025 a las 23:07:38
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cliente&factura`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(48) NOT NULL,
+  `apellidos` varchar(48) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `telefono` int(48) DEFAULT NULL,
+  `correo` varchar(48) NOT NULL,
+  `cedula` int(48) NOT NULL,
+  `genero` varchar(48) NOT NULL,
+  `direccion` varchar(48) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id_mesa` int(48) NOT NULL,
+  `id_cliente` varchar(32) NOT NULL,
+  `id_producto` varchar(32) NOT NULL,
+  `id_empleado` int(32) NOT NULL,
+  `fecha_factura` date DEFAULT current_timestamp(),
+  `forma_pago` varchar(32) NOT NULL,
+  `estado_factura` int(32) NOT NULL,
+  `descuento` int(32) DEFAULT NULL,
+  `propina` int(32) DEFAULT NULL,
+  `costo_total` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
