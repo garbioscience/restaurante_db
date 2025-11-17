4f2262f88b4350413302df6cba55f57f65b053a6
Ivonne-torres-chilito <ivonnech493@gmail.com>
2025-11-16 22:44:56 -0500

    Update 005_producto.sql

--- tablas/005_producto.sql
+++ tablas/005_producto.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE producto (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    unidad_medida VARCHAR(50) NOT NULL,
+    precio_compra DECIMAL(12,2) NOT NULL,
+    precio_venta DECIMAL(12,2) NOT NULL,
+    stock_actual DECIMAL(10,2) NOT NULL DEFAULT 0,
+    proveedor VARCHAR(120),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
