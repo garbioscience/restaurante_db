ecf55a6d2a7bd9f494c2a3ce08268fafac0fc2bf
Jairsiño <jairbarrios686@gmail.com>
2025-11-17 20:06:57 -0500

    001 mesa.sql

--- tablas/001 mesa.sql
+++ tablas/001 mesa.sql
@@ -2,8 +2,8 @@ CREATE TABLE mesa (
     id_mesa INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     estado ENUM('Disponible', 'Reservado') NOT NULL DEFAULT 'Disponible',
-    fecha DATE NOT NULL,
-    hora TIME NOT NULL,
-    cantidad INT UNSIGNED NOT NULL,
+    fecha DATE,
+    hora TIME,
+    cantidad INT,
     tipo ENUM('F', 'P', 'D')
 );
